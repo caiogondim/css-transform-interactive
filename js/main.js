e8a42ec7b9615ba8565f22ef916c1d7599281279
Caio Gondim <me@caiogondim.com>
2013-11-04 21:48:51 -0200

    Adds “Rotate” experiment

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ $('.transform-rule-slider-label input').on('change', function (event) {
   var scaleX = ($('.transform-rule-slider-X input').val() / 1000).toFixed(2)
   var scaleY = ($('.transform-rule-slider-Y input').val() / 1000).toFixed(2)
 
-  // updates the code
   $('.code-rule-value-scale-x').html(scaleX)
   $('.code-rule-value-scale-y').html(scaleY)
 
-  // reflect changes in the experiment
-  $('.transform-rule-img-transformed').css('-webkit-transform', 'scale(' + scaleX + ', ' + scaleY + ')' );
+  $('.transform-rule-img-transformed').css('-webkit-transform', 'scale(' + scaleX + ', ' + scaleY + ')' )
+})
+
+$('#rotate .transform-rule-slider').on('change', function (event) {
+  var rotation = parseFloat($(this).val()).toFixed(2)
+
+  $('#rotate .code-rule-value-rotation').html(rotation)
+
+  $('#rotate .transform-rule-img-transformed').css('-webkit-transform', 'rotate(' + rotation + 'deg)')
 })
