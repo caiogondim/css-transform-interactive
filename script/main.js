4790ecf27b05316808014293f12fca92c9804348
Caio Gondim <me@caiogondim.com>
2013-11-14 23:50:38 -0200

    The end of the endless refactoring

--- script/main.js
+++ script/main.js
@@ -12,15 +12,15 @@ var main = {};
   main.bindEventListeners = function () {
     $('#translate input[type=range]').on(
       'change',
-      main.handleTranslateValueChange
+      this.handleTranslateValueChange
     )
 
     $('#scale input[type=range]').on(
       'change',
-      main.handleScaleValueChange
+      this.handleScaleValueChange
     )
 
-    $('#rotate .transform-rule-slider').on(
+    $('#rotate input[type=range]').on(
       'change',
       this.handleRotateValueChange
     )
@@ -35,26 +35,28 @@ var main = {};
   // --------------
 
   main.handleTranslateValueChange = function () {
-    var translateX = $('#translate .transform-rule-slider-x').val()
-    var translateY = $('#translate .transform-rule-slider-y').val()
+    var translateX = $('#translate .experiment__controls__slider.axis-x').val()
+    var translateY = $('#translate .experiment__controls__slider.axis-y').val()
 
-    $('#translate .code-rule-value-translate-x').html(translateX)
-    $('#translate .code-rule-value-translate-y').html(translateY)
+    $('#translate .experiment__code__rule-value-translate-x').html(translateX)
+    $('#translate .experiment__code__rule-value-translate-y').html(translateY)
 
-    $('#translate .transform-rule-img-transformed').css(
+    $('#translate .experiment__stage__img-transformed').css(
       '-webkit-transform',
       'translate(' + translateX + 'px, ' + translateY + 'px)'
     )
   }
 
   main.handleScaleValueChange = function () {
-    var scaleX = ($('#scale .transform-rule-slider-x').val() / 1000).toFixed(2)
-    var scaleY = ($('#scale .transform-rule-slider-y').val() / 1000).toFixed(2)
+    var scaleX = $('#scale .experiment__controls__slider.axis-x').val()
+    scaleX = (scaleX / 1000).toFixed(2)
+    var scaleY = $('#scale .experiment__controls__slider.axis-y').val()
+    scaleY = (scaleY / 1000).toFixed(2)
 
-    $('#scale .code-rule-value-scale-x').html(scaleX)
-    $('#scale .code-rule-value-scale-y').html(scaleY)
+    $('#scale .experiment__code__rule-value-scale-x').html(scaleX)
+    $('#scale .experiment__code__rule-value-scale-y').html(scaleY)
 
-    $('#scale .transform-rule-img-transformed').css(
+    $('#scale .experiment__stage__img-transformed').css(
       '-webkit-transform',
       'scale(' + scaleX + ', ' + scaleY + ')'
     )
@@ -63,22 +65,24 @@ var main = {};
   main.handleRotateValueChange = function () {
     var rotation = parseFloat($(this).val()).toFixed(2)
 
-    $('#rotate .code-rule-value-rotation').html(rotation)
+    $('#rotate .experiment__code__rule-value-rotation').html(rotation)
 
-    $('#rotate .transform-rule-img-transformed').css(
+    $('#rotate .experiment__stage__img-transformed').css(
       '-webkit-transform',
       'rotate(' + rotation + 'deg)'
     )
   }
 
   main.handleSkewValueChange = function () {
-    var skewX = parseFloat($('#skew .transform-rule-slider-x').val())
-    var skewY = parseFloat($('#skew .transform-rule-slider-y').val())
+    var skewX = $('#skew .experiment__controls__slider.axis-x').val()
+    skewX = parseFloat(skewX)
+    var skewY = $('#skew .experiment__controls__slider.axis-y').val()
+    skewY = parseFloat(skewY)
 
-    $('#skew .code-rule-value-skew-x').html(skewX + 'deg')
-    $('#skew .code-rule-value-skew-y').html(skewY + 'deg')
+    $('#skew .experiment__code__rule-value-skew-x').html(skewX + 'deg')
+    $('#skew .experiment__code__rule-value-skew-y').html(skewY + 'deg')
 
-    $('#skew .transform-rule-img-transformed').css(
+    $('#skew .experiment__stage__img-transformed').css(
       '-webkit-transform',
       'skew(' + skewX + 'deg, ' + skewY + 'deg)'
     )
